294def83c09b2dfe5b247ec634f02ebe1a401208
ianklfong <94506432+ianklfong@users.noreply.github.com>
2022-01-20 23:58:28 +0800

    Add files via upload

--- task-manager/src/Components/TaskList/TaskList(Refactor).js
+++ task-manager/src/Components/TaskList/TaskList(Refactor).js
@@ -1,5 +1,6 @@
 // import React
 import React from "react";
+// import { SortablePane, Pane } from 'react-sortable-pane';
 
 // import stylesheet
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,7 +10,9 @@ import './TaskList.css';
 // import Task from '../Task/Task';
 import Task from '../Task/Task(Refactor)';
 
-export default function TaskList({ allTasks }) {    
+export default function TaskList({ allTasks, removeDone }) {   
+    //  draggable panes for individual taks
+    //  const panes = allTasks.map((task, index) =><Pane key={index} defaultSize={{ width: '100%', height: 120 }}><div className="list-group-item"><Task task={task} /></div></Pane>)
 
     return (
         <div className="row h-100">
@@ -26,15 +29,15 @@ export default function TaskList({ allTasks }) {
                 {/* <!-- Start of lower part: List --> */}
                 <div className="row">
                     <div className="btnClass">
-                        <div className="dropdown">
+                        {/* <div className="dropdown">
                             <button className="dropbtn  fas fa-chevron-down">SORT BY</button>
                             <div className="dropdown-content">
                                 <a href="#">A-Z</a>
                                 <a href="#">Z-A</a>
                             </div>
-                        </div>
+                        </div> */}
                         <div className="remove">
-                            <button className="removebtn">REMOVE DONE</button>
+                            <button className="removebtn" onClick={removeDone}>REMOVE DONE</button>
                         </div>
                     </div>
                     {/* <!-- Start of main column --> */}
@@ -49,6 +52,11 @@ export default function TaskList({ allTasks }) {
                             </div>
 
                         </ul>
+                        {/* containers for draggable tasks panes */}
+                        {/* <div>
+                        <SortablePane direction="vertical" margin={100}>{panes}</SortablePane>
+                    </div> */}
+
                     </div>
                 </div>
             </div>
